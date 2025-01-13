7e61b9d61ca6ff81eb2319e7273b27808875ac82
abdessattar23 <elyagoubiabdessattar@gmail.com>
2025-01-13 11:15:44 +0100

    YOU-9 Database scheme v1

---
+++ scheme.sql
@@ -0,0 +1,75 @@
+CREATE DATABASE IF NOT EXISTS Youdemy;
+USE Youdemy;
+
+-- Creating Users Table
+
+CREATE TABLE IF NOT EXISTS Users (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    role ENUM('admin', 'student', 'teacher') NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+-- Creating Categories Table
+
+CREATE TABLE IF NOT EXISTS Categories (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+-- Creating Courses Table
+
+CREATE TABLE IF NOT EXISTS Courses (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    content TEXT NOT NULL,
+    teacher_id INT NOT NULL,
+    category_id INT NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (teacher_id) REFERENCES Users(id),
+    FOREIGN KEY (category_id) REFERENCES Categories(id)
+);
+
+-- Creating Tags Table
+
+CREATE TABLE IF NOT EXISTS Tags (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VarChar(100) NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+-- Creating Course-Tags Table
+
+CREATE TABLE IF NOT EXISTS CourseTags (
+    tag_id INT NOT NULL,
+    course_id INT NOT NULL,
+    FOREIGN KEY (tag_id) REFERENCES Tags(id),
+    FOREIGN KEY (course_id) REFERENCES Courses(id)
+);
+
+-- Creating Enrolment Tbale
+
+CREATE TABLE IF NOT EXISTS Enrolments (
+    id INT NOT NULL AUTO_INCREMENT,
+    student_id INT NOT NULL,
+    course_id INT NOT NULL,
+    enrolled_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (student_id) REFERENCES Users(id),
+    FOREIGN KEY (course_id) REFERENCES Courses(id)
+);
+
+
